c2a83a0347ba881964f5d5f75aa9b78cf501bc4e
itbertolaso <marco.bertolaso@tencosuperiorekennedy.it>
2020-01-12 13:09:16 +0100

    Minor qol upgrades

--- src/app/pages/dettaglio-dipendenti-page/dettaglio-dipendenti-page.component.ts
+++ src/app/pages/dettaglio-dipendenti-page/dettaglio-dipendenti-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { DipendentiService } from "src/core/service/dipendenti.service";
+import { DomainService } from "src/core/service/domain.service";
 
 @Component({
   selector: "app-dettaglio-dipendenti-page",
@@ -11,13 +12,18 @@ export class DettaglioDipendentiPageComponent implements OnInit {
   soggetto;
   constructor(
     public routeActive: ActivatedRoute,
-    public dipendente: DipendentiService
+    public dipendente: DipendentiService,
+    public country: DomainService
   ) {}
 
   ngOnInit() {
     const id = this.routeActive.snapshot.params.id;
     this.dipendente.getById(id).subscribe(res => {
-      this.soggetto = res;
+      this.country.getByIso(res.country).subscribe(cc => {
+        let s = { ...res, country: cc[0].description };
+        console.log(cc[0].description);
+        this.soggetto = s;
+      });
     });
   }
 }
