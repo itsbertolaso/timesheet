d9ff65a978cf928213b28973316a7fd4653fa585
itbertolaso <marco.bertolaso@tencosuperiorekennedy.it>
2020-01-12 13:37:49 +0100

    removed junk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,44 +8,6 @@ import { DipendentiService } from "src/core/service/dipendenti.service";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  constructor(public dipendente: DipendentiService) {}
-
-  @Output() resetSelectionListener: EventEmitter<any> = new EventEmitter<any>();
-
-  options: DataTableOptions = {
-    colsOptions: [
-      {
-        label: "Nome",
-        name: "nome"
-      },
-      {
-        label: "Regione",
-        name: "regione"
-      },
-      {
-        label: "Sesso",
-        name: "sex"
-      }
-    ]
-  };
-
-  currentIndex: number;
-  public isVisible: boolean = true;
+  constructor() {}
   public title: string = "timesheet";
-  // public listanomi: string[] = ["gesu", "giuseppe", "maria"];
-
-  currentSelection = [];
-  listaOutput: any[] = [];
-
-  toggle() {
-    this.isVisible = !this.isVisible;
-  }
-
-  select(input: any, index: number) {
-    this.currentSelection[index] = input;
-  }
-
-  resetSelectionEmitter() {
-    this.resetSelectionListener.emit(null);
-  }
 }
